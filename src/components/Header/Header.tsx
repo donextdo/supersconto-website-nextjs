6eb1eba992c72ac483f52b6815c4ac72a955540a
Githendra McShane <githendramax@gmail.com>
2022-12-09 10:26:09 +0530

    Initial commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface HeaderProps {
+    // TODO
+}
+
+function Header(props: HeaderProps) {
+    return (
+        <div>Header Component</div>
+    );
+}
+
+export default Header;
