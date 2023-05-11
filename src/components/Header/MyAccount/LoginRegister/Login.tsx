1ac7a0c5f07068ff3f5bce115af887c5fc41b5e8
Thisara Thathsarani <thisararpg@gmail.com>
2023-05-11 16:53:46 +0530

    add validation to login register

--- src/components/Header/MyAccount/LoginRegister/Login.tsx
+++ src/components/Header/MyAccount/LoginRegister/Login.tsx
@@ -1,10 +1,9 @@
-
-import  { useRouter } from "next/router";
+import { useRouter } from "next/router";
 import React, { useState } from "react";
 import { http } from "../../../../../utils/request";
 import Swal from "sweetalert2";
 import { ToastContainer, toast } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 
 type FormValues = {
   usernameoremail: string;
@@ -18,26 +17,26 @@ type Props = {
 const Login: React.FC<Props> = () => {
   const [usernameoremail, setUsernameoremail] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMsg, setErrorMsg] = useState<React.ReactNode>("");
   const router = useRouter();
-  
+
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const details = {
-      email:usernameoremail,
-      password:password,
-      
-  }
-  
+      email: usernameoremail,
+      password: password,
+    };
+
     try {
       const response = await http.post(`/users/login`, details);
       console.log(response);
 
-      localStorage.setItem('token', response.data.token)
-      localStorage.setItem('id', response.data._id)
-      localStorage.setItem('email', response.data.email)
+      localStorage.setItem("token", response.data.token);
+      localStorage.setItem("id", response.data._id);
+      localStorage.setItem("email", response.data.email);
 
-
-      if(response.status==200){
+      if (response.status == 200) {
         //  location.reload();
         // toast.success('You have successfully logged in.', {
         //   position: "top-right",
@@ -47,10 +46,10 @@ const Login: React.FC<Props> = () => {
         //   pauseOnHover: true,
         //   draggable: true,
         //   progress: undefined,
-          
+
         // });
         // router.push('/account');
-        toast.success('You have successfully logged in.', {
+        toast.success("You have successfully logged in.", {
           position: "top-right",
           autoClose: 2000,
           hideProgressBar: true,
@@ -60,22 +59,30 @@ const Login: React.FC<Props> = () => {
           progress: undefined,
         });
         setTimeout(() => {
-          router.push('/account');
+          router.push("/account");
         }, 2000);
       }
-
-    } catch (error:any) {
+    } catch (error: any) {
       console.log(error);
       Swal.fire({
-        title: 'Error',
+        title: "Error",
         text: error.message,
-        icon: 'error',
-        confirmButtonText: 'Done',
-        confirmButtonColor: '#8DC14F',
-        
-      })
+        icon: "error",
+        confirmButtonText: "Done",
+        confirmButtonColor: "#8DC14F",
+      });
     }
   };
+
+  async function handleClick() {
+    setIsLoading(true);
+    if (!usernameoremail) {
+      setErrorMsg("Username is required.");
+    } else if (!password) {
+      setErrorMsg("The password field is empty.");
+    }
+    setIsLoading(false);
+  }
   return (
     <>
       <div className="max-w-lg mx-auto  border-t-0 ">
@@ -95,7 +102,7 @@ const Login: React.FC<Props> = () => {
                   name="username-email"
                   id="username-email"
                   autoComplete="given-username-email"
-                  required 
+                  required
                   className="block w-full border-0 py-2 px-3.5 text-gray-900 bg-[#f3f4f7] "
                   value={usernameoremail}
                   onChange={(e) => setUsernameoremail(e.target.value)}
@@ -112,10 +119,10 @@ const Login: React.FC<Props> = () => {
                   name="password"
                   id="password"
                   autoComplete="password"
-                  required 
+                  required
                   className="block w-full border-0 py-2 px-3.5 text-gray-900 bg-[#f3f4f7]"
                   value={password}
-                   onChange={(e) => setPassword(e.target.value)}
+                  onChange={(e) => setPassword(e.target.value)}
                 />
               </div>
             </div>
@@ -129,12 +136,21 @@ const Login: React.FC<Props> = () => {
           <div className="mx-2 mt-5 mb-10 ">
             <button
               type="submit"
-              className=" rounded-md w-full block bg-[#8DC14F] px-3.5 py-2.5 text-center text-sm font-semibold text-white"
+              className={`rounded-md w-full block bg-[#8DC14F] px-3.5 py-2.5 text-center text-sm font-semibold text-white${
+                isLoading ? "loading" : ""
+              }`}
+              onClick={handleClick}
             >
-              Login
+              {isLoading ? "Loading..." : "Login"}
             </button>
             <ToastContainer />
           </div>
+          {errorMsg && (
+            <div className="border border-gray-300 p-3 mx-2 flex items-center">
+              <span className="font-bold text-black mr-2">Error:</span>
+              <div className="text-black text-sm">{errorMsg}</div>
+            </div>
+          )}
         </form>
       </div>
     </>
